382ebe0d0bd97fbaf20577065824418e48554fc5
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-06-22 21:38:51 +0600

    Writing first test in the cypress automation

---
+++ cypress/e2e/Test1.cy.js
@@ -0,0 +1,12 @@
+describe('My first test', () => {
+    it('test1 verify title-positive', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/') // change URL to match your dev URL
+      cy.title().should('eq','OrangeHRM')
+    
+    })
+    it('test2 verify title-negative', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/') // change URL to match your dev URL
+        cy.title().should('eq','OrangeHRM123')
+      
+      })
+  })
