4bfb7e70fd8250e9eafe53d2e29df600526cb815
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-06-24 21:07:07 +0600

    interact with input field automation

--- cypress/e2e/clickIntegration.cy.js
+++ cypress/e2e/clickIntegration.cy.js
@@ -1,1 +1,15 @@
 /// <reference types='cypress' />
+
+describe('Test 1 interact with input field', () => {
+    it('Visiting the google page', () => {
+        cy.visit('https://google.com');
+        
+    });
+    it('click on the text field', () => {
+        cy.visit('https://google.com');
+        cy.get("[name='q']").click();
+        cy.get('.erkvQe > .OBMEnb').should("be.visible").contains("Trending Searches");
+        cy.get("[class='lnXdpd']").click();
+    });
+
+});
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -3,14 +3,13 @@
 describe('learn about the locators', () => {
     it('visit google home page', () => {
         cy.visit('https://www.google.com/')
-    });
-    it('can locate button on the page', () => {
         cy.get('[name="q"]')
         .should("be.visible")
         .and("have.class", "gLFyf")
         .and("have.value", "")
         .and("have.attr", "data-ved", "0ahUKEwiRoPn6_tv_AhXQplYBHVI5DToQ39UDCAQ")
     });
+
     
 
 });
