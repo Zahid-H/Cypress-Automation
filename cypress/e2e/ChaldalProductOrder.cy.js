9a43f1937fba66cb034f6b8ff5cee8a0a1107ad4
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-06-27 22:24:34 +0600

    chaldal url automation working fine

---
+++ cypress/e2e/ChaldalProductOrder.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Automation of chaldal Multiple product order', () => {
+    
+    beforeEach(() => {
+        //visiting the the chaldal URL 
+        cy.visit('https://chaldal.com/')
+    });
+    it('', () => {
+        
+    });
+});
